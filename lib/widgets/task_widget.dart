fa4e92b9fb4a3554bbd843aeaf0fe43cadcca9ad
sina <sina6g@gmail.com>
2022-04-06 14:38:42 +0430

    task widget

---
+++ lib/widgets/task_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+class TaskWidget extends StatelessWidget {
+  final String text;
+  final Color? color;
+  const TaskWidget({Key? key,
+    required this.text,
+    required this.color
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      height: MediaQuery.of(context).size.height/14,
+      decoration: BoxDecoration(
+        color: Color(0xFFedf0f8),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(text, style: TextStyle(
+            fontSize: 20,
+            color: color
+        ),),
+      ),
+    );
+  }
+}
